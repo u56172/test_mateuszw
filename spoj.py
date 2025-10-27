529abca29ff5a84cbcc16387b8b42f3b5ffea1eb
Adiaxi <u56129@student.wat.edu.pl>
2025-10-27 21:03:35 +0100

    feat: add script for 2 digits sum

---
+++ spoj.py
@@ -0,0 +1,7 @@
+# Napisać program do dodawania dwóch liczb całkowitych. Na wejściu podane są w oddzielnych liniach dwie liczby
+# naturalne A oraz B mniejsze od 200. Na wyjściu należy wypisać wartość ich sumy, A + B.
+
+zmienna_a:int=int(input())
+zmienna_b:int=int(input())
+print(zmienna_a+zmienna_b)
+
